db87a2736d2f784a844346effb1a78d65e6df2fc
Chris <electronicsleep@gmail.com>
2018-01-30 19:42:45 -0800

    Debian desktop

---
+++ debian-desktop.sh
@@ -0,0 +1,73 @@
+### UBUNTU 16 / DEBIAN 9
+
+# tasksel
+
+# install gnome - laptop tools
+
+### CLI BASED
+
+apt update
+
+apt upgrade -y
+
+apt install ssh net-tools -y 
+
+apt install screenfetch -y
+
+apt install htop tree ssh fail2ban rsync git -y
+
+apt install screen nload iotop vim irssi wget lynx tmux -y
+
+apt install tcpdump shellcheck bash-completion -y
+
+### SIMPLE
+
+apt install vim-gnome terminator conky gparted -y
+
+### MORE
+
+apt install thunar rhythmbox synaptic -y
+
+apt install keepassx inkscape vlc -y
+
+apt install libreoffice -y
+
+apt install gimp darktable digikam krita mypaint dia gpaint blender -y
+
+apt install freecad librecad filezilla -y
+
+apt install gnome-builder -y
+
+apt install fontforge -y
+
+apt install mixxx lmms -y
+
+apt installl ardour audicity xmms
+
+apt-get install hexchat wireshark nmap pidgin -y
+
+apt-get install irssi-scripts vim-doc vim-scripts -y
+
+### UNITY3d
+
+apt-get install monodevelop mono-reference-assemblies-* -y
+
+### FOR UNREAL
+
+apt-get install codelite codelite-plugins -y
+
+### KDE
+
+apt-get install kdevelop qtcreator -y
+
+### INSTALL YOURSELF
+
+#chrome pycharm intellij
+#atom sublimetext2
+
+# install ~/.tmux.conf
+# install ~/.vimrc
+
+apt install eclipse eclipse-cdt -y
+
+apt install gradle -y
