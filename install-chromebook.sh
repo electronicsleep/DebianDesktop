f5c38df6a5601c4c75edafb67a53a9695f2a7f0f
Chris <electronicsleep@gmail.com>
2018-09-19 00:01:44 -0700

    install chromebook instructions

--- install-chromebook.sh
+++ install-chromebook.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+#Tested on Asus C202S notebook
 
 echo "
 Step 1) Change chromebook to dev
 
 https://support.google.com/chromebook/answer/1086915?hl=en
+
 click photo -> settings gear -> options button -> about chromeos -> detailed build info
 -> change channel to dev
 
-Step 2) settings -> install linux container
+Step 2) settings -> install Linux container
 
-Step 3) install linux software
+Step 3) install GNU/Linux software
 "
+
+read -p "Ready to install? (y/n) " -n 1 -r
+echo    
+ if [[ $REPLY =~ ^[Yy]$ ]]
+then
 
 sudo apt-get update && sudo apt-get upgrade
 
@@ -25,3 +32,5 @@ sudo apt-get install keepassx vim-gnome -y
 sudo apt-get keepassx vlc hexchat -y
 
 sudo apt-get install gnome-builder -y
+
+fi
