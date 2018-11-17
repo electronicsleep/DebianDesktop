4889da838104e13c3d1614d225b796e32ea8d028
Chris <electronicsleep@gmail.com>
2018-11-17 15:43:17 -0800

    install-ubuntu-air

---
+++ install-ubuntu-air.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# MacBook Air (13-inch early 2015)
+set -e
+
+apt-get update
+apt-get upgrade
+apt-get install git -y
+
+# Repo
+# https://github.com/electronicsleep/DebianDesktop
+
+read -p "Install cli programs? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+apt-get install net-tools tree htop -y
+apt-get install tmux vim rsync -y
+#apt-get install openssh-server -y
+
+fi
+
+read -p "Install gui programs? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+apt-get install terminator -y
+apt-get install keepassx -y
+apt-get install openjdk-8-jdk -y
+apt purge ubuntu-web-launchers -y
+
+apt-get install ardour -y
+apt-get install audacity -y
+apt-get install vlc -y
+apt-get install lmms -y
+
+apt-get install gimp inkscape krita blender darktable -y
+
+apt-get install gnome-tweaks -y
+
+apt-get install gnome-builder vim-gnome -y
+
+# install spotify (snap)
+# install pycharm (snap)
+# install atom (snap)
+# install sublime text (snap)
+# install meld (snap)
+# install intellij (snap)
+# install godot (snap)
+
+# install golang
