188e647774053a31b376e2afd5f72b1dac03c930
Chris <electronicsleep@gmail.com>
2018-11-17 15:51:49 -0800

    install-ubuntu-air

--- install-ubuntu-air.sh
+++ install-ubuntu-air.sh
@@ -27,18 +27,22 @@ then
 
 apt-get install terminator -y
 apt-get install keepassx -y
+apt-get install gnome-tweaks -y
 apt-get install openjdk-8-jdk -y
 apt purge ubuntu-web-launchers -y
 
+# Audio
 apt-get install ardour -y
 apt-get install audacity -y
 apt-get install vlc -y
 apt-get install lmms -y
+apt-get install hydrogen -y
+apt-get install terminatorx -y
 
+# Graphics
 apt-get install gimp inkscape krita blender darktable -y
 
-apt-get install gnome-tweaks -y
-
+# Development
 apt-get install gnome-builder vim-gnome -y
 
 fi
