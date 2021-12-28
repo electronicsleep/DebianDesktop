1ac6e6adf20e8837ac07269bf9343b723da7f883
Chris <electronicsleep@gmail.com>
2021-12-28 12:42:55 -0800

    update-install-ubuntu-air

--- install-ubuntu-air.sh
+++ install-ubuntu-air.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 # MacBook Air (13-inch early 2015)
-# Ubuntu 18.04 bionic
-set -e
+# Ubuntu 20.04 focal
+
+# Repo:
+# https://github.com/electronicsleep/DebianDesktop
+# https://github.com/electronicsleep/DebianDesktop/blob/master/install-ubuntu-air.sh
+# https://github.com/electronicsleep/DebianDesktop/blob/master/mac-keys.txt
+
+set -ex
 
 apt-get update
 apt-get upgrade
-apt-get install git -y
-
-# Repo
-# https://github.com/electronicsleep/DebianDesktop
+apt-get install git vim curl -y
 
 read -p "Install cli programs? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-apt-get install net-tools tree htop -y
-apt-get install tmux vim rsync curl -y
-#apt-get install openssh-server -y
+    apt-get install net-tools tree htop -y
+    apt-get install tmux vim rsync -y
 
 fi
 
@@ -26,33 +28,28 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-apt-get install terminator -y
-apt-get install keepassx -y
-apt-get install gnome-tweaks -y
-apt purge ubuntu-web-launchers -y
+    apt-get install terminator -y
+    apt-get install keepassx -y
+    apt-get install gnome-tweaks -y
 
-# Audio
-apt-get install vlc -y
-#apt-get install ardour -y
-#apt-get install audacity -y
-#apt-get install lmms -y
-#apt-get install hydrogen -y
-#apt-get install terminatorx -y
+    # Audio
+    apt-get install audacity -y
+    apt-get install vlc -y
+    apt-get install lmms -y
+    apt-get install hydrogen -y
 
-# Graphics
-apt-get install gimp inkscape krita blender darktable -y
+    # Graphics
+    apt-get install gimp inkscape krita blender darktable -y
 
-# Development
-apt-get install gnome-builder vim-gnome -y
-apt-get install gitg
-#apt-get install openjdk-8-jdk -y
+    # Development
+    apt-get install gnome-builder gitg -y
+    apt-get install python3 python3-pip golang -y
 
 fi
 
 # install spotify (snap)
-# install pycharm (snap)
-# install atom (snap)
 # install sublime text (snap)
 # install meld (snap)
+# install pycharm (snap)
 # install intellij (snap)
 # install godot (snap)
